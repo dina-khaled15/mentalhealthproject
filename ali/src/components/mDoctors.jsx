828eb062d8b7a66db65aa85122702538a398d7df
Ali Omar <168965213+AliOmar104@users.noreply.github.com>
2025-04-14 15:43:27 +0200

    Add files via upload

---
+++ ali/src/components/mDoctors.jsx
@@ -0,0 +1,11 @@
+// File: components/TitleSection.jsx
+import React from 'react';
+
+export default function TitleSection() {
+  return (
+    <div style={{ padding: '20px' }}>
+      <h1>Doctors</h1>
+      <p>Meat Our Doctors</p>
+    </div>
+  );
+}
