9b86aaa05489dfe689ea70bff64830a77736a0d3
dina-khaled15 <20224421@fue.edu.eg>
2025-04-15 22:24:50 +0200

    First commit - upload doctorprofile project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import DoctorPage from './page/DoctorPage';
+
+function App() {
+  return (
+    <div style={{ overflowX: 'hidden' }}>
+      <DoctorPage />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/data/doctorData.js
@@ -0,0 +1,60 @@
+// src/data/doctorData.js
+
+const doctorData = {
+    name: "John Ramirez, LCSW",
+    title: "Psychiatrist",
+    description:
+      "A Licensed Clinical Social Worker (LCSW) with over 12 years of experience in providing compassionate counseling and therapy for individuals and families. He specializes in trauma recovery, family dynamics, and supporting individuals through life transitions. John's approach is rooted in empathy, with a focus on fostering healing and resilience.",
+    expertise: [
+      "Family Counseling",
+      "Trauma Recovery",
+      "PTSD",
+      "Grief and Loss Therapy",
+      "Conflict Resolution",
+    ],
+    contact: {
+      phone: "(123) 456-7890",
+      email: "john.ramirez@wellthy.com",
+    },
+    socialMedia: ["LinkedIn", "Twitter", "Facebook", "Instagram"],
+    education: [
+      {
+        year: "2011 - 2013",
+        degree: "Master of Social Work (MSW)",
+        school: "University of Southern California",
+      },
+      {
+        year: "2006 - 2010",
+        degree: "Bachelor of Psychology",
+        school: "California State University",
+      },
+    ],
+    experience: [
+      {
+        year: "2015 - Present",
+        role: "Senior Therapist",
+        place: "Wellthy",
+      },
+      {
+        year: "2012 - 2015",
+        role: "Clinical Social Worker",
+        place: "Hope Counseling Center",
+      },
+      {
+        year: "2010 - 2012",
+        role: "Licensed Social Worker",
+        place: "Community Health Clinic",
+      },
+    ],
+    certificates: [
+      "Licensed Clinical Social Worker (LCSW) Certification",
+      "Certified in Trauma-Focused Cognitive Behavioral Therapy",
+      "Certified Family Therapist",
+    ],
+    avatar: "/doctor.png",
+    
+
+  };
+  
+  export default doctorData;
+  
