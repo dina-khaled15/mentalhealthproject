7e5b4e593c9290e6dcac03bc5e9a460ff867bfd3
Aya <ayahanymohamedfahim@gmail.com>
2025-04-21 14:22:02 +0200

    cont

--- src/components/service.jsx
+++ src/components/service.jsx
@@ -11,31 +11,35 @@ import {
 import PolylineIcon from "@mui/icons-material/Polyline";
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 import ArrowBackIosNewIcon from "@mui/icons-material/ArrowBackIosNew";
+import picture1 from "../images/Picture1.png";
+import picture2 from "../images/Picture2.png";
+import picture3 from "../images/Picture3.jpg";
+import picture4 from "../images/Picture.png";
 
 const services = [
   {
     title: "Individual Therapy",
     description:
       "Personalized one-on-one sessions with licensed therapists to address your unique mental health needs.",
-    image: "../images/Picture1.png",
+    image: picture1,
   },
   {
     title: "Online Counseling",
     description:
       "Convenient and confidential virtual therapy sessions that you can attend from the comfort of your home.",
-    image: "../images/Picture2.png",
+    image: picture2,
   },
   {
     title: "Group Therapy",
     description:
       "Supportive group sessions where individuals can share experiences and learn from another in a safe place.",
-    image: "../images/Picture3.jpg",
+    image: picture3,
   },
   {
     title: "Mindfulness and Meditation",
     description:
       "Techniques and practices to help you stay present, reduce stress, and enhance emotional regulation.",
-    image: "../images/Picture.png",
+    image: picture4,
   },
 ];
 
