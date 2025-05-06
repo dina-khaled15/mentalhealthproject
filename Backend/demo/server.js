d7f807d187c0419916b3305f217b26df11963643
Malak <ma4409533@gmail.com>
2025-05-06 04:57:10 +0300

    Backend

--- Backend/demo/server.js
+++ Backend/demo/server.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 require('dotenv').config();
 const userRouter = require('./routers/userfeedback.routes');
+const userIssuesRouter = require('./routers/userIssues.routes');
+const userPharmacyRouter = require('./routers/userPharmacy.routes');
+const userEmotionsroutes = require('./routers/userEmotions.routes');
+const userBookingroutes= require('./routers/userBooking.routes');
 const errorHandler = require('./middlewares/errorHandler.middleware');
 const loggingMiddleware = require('./middlewares/loganthing.middleware');
 const connectDB = require('./config/db');
@@ -13,7 +17,10 @@ app.use(express.json());
 // app.use(loggingMiddleware);
 
 app.use('/feedback', userRouter);
-
+app.use('/Issues', userIssuesRouter);
+app.use('/Pharmacy', userPharmacyRouter);
+app.use('/Emotions', userEmotionsroutes);
+app.use('/Booking', userBookingroutes);
 
 app.use(errorHandler);
 
